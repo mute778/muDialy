4a3262117acfb921071052fe679debd1329abd69
mute778 <mute.develop@gmail.com>
2026-06-07 12:36:41 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,26 @@
-const CACHE_NAME = "daily-memo-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
+const CACHE_NAME = 'diary-app-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// インストール時にアセットをキャッシュ
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-// 古いキャッシュを削除
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// ネットワーク優先・失敗時にキャッシュから返す
-self.addEventListener("fetch", (event) => {
-  // Slack / Cloudflare Worker へのリクエストはキャッシュしない
-  if (!event.request.url.startsWith(self.location.origin)) {
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request)
-      .then((res) => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
-        return res;
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
       })
-      .catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
